167982c3c39b1fd5edd91b9a31b487173a72c396
Eduardo@42sp <tardineduardo@gmail.com>
2025-09-03 21:16:41 -0300

    done

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,17 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/03 21:01:25 by eduribei          #+#    #+#             */
-/*   Updated: 2025/09/03 21:01:27 by eduribei         ###   ########.fr       */
+/*   Created: 2025/09/03 21:04:02 by eduribei          #+#    #+#             */
+/*   Updated: 2025/09/03 21:14:55 by eduribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <string>
+#include <iostream>
 
-Zombie* newZombie(std::string name) {
-    return new Zombie(name);
+int main(void)
+{
+	std::string brain = "HI THIS IS BRAIN";
+	std::string* stringPTR = &brain;
+	std::string& stringREF = brain;
+	std::cout << brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
