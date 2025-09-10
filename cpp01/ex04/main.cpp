0cc35df74a4a27e55b69f13749e5cb040de64e83
Eduardo@42sp <tardineduardo@gmail.com>
2025-09-10 19:56:07 -0300

    done ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,45 +5,56 @@
 
 int	main(int argc, char **argv)
 {
-	std::ifstream	inFile;
-	std::string		content;
-	std::string		newstr;
-	std::string		line;
-	size_t			len1;
-	size_t			startpos;
+	std::string		contentsrc;
+	std::string		contentdst;
 
 	//checking number of args
-	if (argc != 3) {
+	if (argc != 4) {
 		std::cout << "invalid number of arguments" << std::endl;
 		return (1);}
 
 	//setting variable values
-	std::string	str1(argv[2]);
-	std::string	str2(argv[3]);
-	len1 = str1.length();
-	startpos = 0;
-	inFile.open(argv[1]);
+	std::string str1 = argv[2];
+	std::string str2 = argv[3];
+	size_t len1 = str1.length();
+	size_t startpos = 0;
 
 	//validating file open
-	if (inFile.fail()) {
-		std::cout << "error opening file" << std::endl;
+	std::ifstream inflile;
+	inflile.open(argv[1]);
+	if (inflile.fail()) {
+		std::cout << "error reading from file" << std::endl;
 		return (1);}
 
 	//read from file
-	while (getline(inFile, line)) {
-		content += line + "\n";}
-	inFile.close();
+	std::string line;
+	while (getline(inflile, line)) {
+		contentsrc += line + "\n";}
+	inflile.close();
 
 	//substituition loop
 	while (true) {
-		size_t pos = content.find(str1, startpos);
+		size_t pos = contentsrc.find(str1, startpos);
 		if (pos == std::string::npos)
 			break;
-		newstr += content.substr(startpos, pos - startpos);
-		newstr += str2;
+		contentdst += contentsrc.substr(startpos, pos - startpos);
+		contentdst += str2;
 		startpos = pos + len1; }
-	newstr += content.substr(startpos);
+	contentdst += contentsrc.substr(startpos);
 
-	std::cout << newstr << std::endl;
+	//open outfile
+	std::ofstream outflile;
+	outflile.open(argv[1]);
+	if (outflile.fail()) {
+		std::cout << "error writing to file" << std::endl;
+		return (1);}	
 
+	//write to outfile
+	outflile << contentdst;
+	if (outflile.fail()) {
+		std::cout << "error writing to file" << std::endl;
+		return (1);}
+	
+	outflile.close();
+	return (0);	
 }
