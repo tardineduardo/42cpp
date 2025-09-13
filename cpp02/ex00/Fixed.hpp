adad13db7cbcf4b765eaf98ac970156fa3079698
Eduardo@42sp <tardineduardo@gmail.com>
2025-09-13 15:50:47 -0300

    cpp02 ex00 done

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,23 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+	private:
+		int					value_;
+		static const int	bits_ = 8;
+
+	public:
+		//canonicals
+		Fixed();								//default constructor
+		~Fixed();								//destructor
+		Fixed(const Fixed& fixed);				//default copy constr
+		Fixed& operator=(const Fixed& other);	//copy assignment op.
+		
+		//from subject
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+#endif
