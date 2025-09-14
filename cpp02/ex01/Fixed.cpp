85f1d0446ca379f1ff7d69923b2cbefdf078d91e
Eduardo@AcerLinux <tardineduardo@gmail.com>
2025-09-13 23:16:38 -0300

    .

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -32,3 +32,36 @@ void Fixed::setRawBits(int const raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->value_ = raw;
 }
+
+
+
+
+//new to ex01
+
+//1. A constructor that takes a constant integer as a parameter. It converts it to the corresponding fixed-point value. The fractional bits value should be initialized to 8, like in exercise 00.
+Fixed::Fixed(const int value) {
+	std::cout << "Constructor #2 called" << std::endl;
+	this->setRawBits(value);
+}
+
+//2. A constructor that takes a constant floating-point number as a parameter. It converts it to the corresponding fixed-point value. The fractional bits value should be initialized to 8, like in exercise 00.
+Fixed::Fixed(const float value) {
+	std::cout << "Constructor #3 called" << std::endl;
+	this->setRawBits(value);
+}
+
+//3. A member function float toFloat( void ) const; that converts the fixed-point value to a floating-point value.
+float Fixed::toFloat(void) const {
+	return 0.0;
+}
+
+//4. A member function int toInt( void ) const; that converts the fixed-point value to an integer value.
+int Fixed::toInt(void) const {
+	return 0;
+}
+
+//5. And add the following function to the Fixed class files: An overload of the insertion («) operator that inserts a floating-point representation of the fixed-point number into the output stream object passed as a parameter.
+float Fixed::operator<<(const Fixed& other) {
+	std::cout << "<< operator called" << std::endl;
+	return other.toFloat();
+}
