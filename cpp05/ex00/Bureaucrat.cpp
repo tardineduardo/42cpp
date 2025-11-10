5af8d86d7b93ab72cd44e66267a3a336a4e849ef
Eduardo@42sp <tardineduardo@gmail.com>
2025-11-10 19:02:07 -0300

    .

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,18 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduribei <eduribei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 18:58:27 by eduribei          #+#    #+#             */
+/*   Updated: 2025/11/10 19:01:57 by eduribei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
 // Default constructor
 Bureaucrat::Bureaucrat(void)
 {
     std::cout << "Default constructor called" << std::endl;
-    return ;
+    //
+	return;
 }
 
 // Copy constructor
 Bureaucrat::Bureaucrat(const Bureaucrat &other)
 {
     std::cout << "Copy constructor called" << std::endl;
-    (void) other;
-    return ;
+    (void)other;
+    return;
 }
 
 // Assignment operator overload
